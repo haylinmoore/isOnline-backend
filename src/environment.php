acf1882984b7d9f42b602bbf2fb848eba1b5a303
chooper100 <chooper100.scratch@gmail.com>
2017-04-13 13:01:31 +0100

    Add global.php and environment.php stubs

---
+++ src/environment.php
@@ -0,0 +1,9 @@
+<?php
+// Site settings
+define('site_host', 'DOMAIN NAME');
+
+// Database connection details
+define('mysqli_host', 'HOST NAME FOR MYSQL DATABASE');
+define('mysqli_user', 'USERNAME FOR MYSQL DATABASE');
+define('mysqli_pass', 'PASSWORD FOR MYSQL DATABASE');
+define('mysqli_database', 'NAME FOR MYSQL DATABASE');
---
+++ src/global.php
@@ -0,0 +1,11 @@
+<?php
+// ...
+
+// Environment settings
+require_once 'environment.php';
+
+// HTTP methods
+define('HTTP_GET', 'GET');
+define('HTTP_POST', 'POST');
+
+// ...
